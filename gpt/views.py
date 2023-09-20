08deb0845480702fcaaf2b9b7dba815b5eb1b1f2
aenstarax <12995192+aenstarax@user.noreply.gitee.com>
2023-09-21 03:10:24 +0800

    UploadChatGpt

--- gpt/views.py
+++ gpt/views.py
@@ -1,20 +1,25 @@
+#gpt-view
 from django.shortcuts import render
 import openai
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.http import JsonResponse
 import json
+from user.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 messages = []
 
+@login_required(login_url='user:login')
 def gpt(request):
-    default_avatar = {"avatar": "media/user/avatar/txdefault.jpg",}
+    user = User.objects.get(username=request.user.username)
     context = {
-        "user": default_avatar,
+        "user": user,
     }
     if request.method == 'GET':
         return render(request, 'gpt/gpt.html', context)
     elif request.method == 'POST':
-        openai.api_key="sk-U0WLocu4KWrrrh678f6d82F522D34042A6Ce78D6C26eA921"
+        openai.api_key=""
         openai.api_base = "https://api.ai-yyds.com/v1"
         json_data = json.loads(request.body.decode("utf-8"))
         user_input = json_data.get("user_input")
