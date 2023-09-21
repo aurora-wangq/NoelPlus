5ce8838d47f246f08ffe8da4a69d96d39cfe460e
aenstarax <12995192+aenstarax@user.noreply.gitee.com>
2023-09-21 18:36:10 +0800

    UploadChatGpt

--- gpt/views.py
+++ gpt/views.py
@@ -7,9 +7,27 @@ import json
 from user.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+import markdown
 
-messages = []
+def mdconv(text: str):
+    md = markdown.Markdown(
+        extensions=[
+            'markdown.extensions.codehilite',
+            'markdown.extensions.meta',
+            'markdown.extensions.extra',
+            'pymdownx.arithmatex'
+        ],
+        extension_configs={
+            'pymdownx.arithmatex': {
+                'generic': True
+            }
+        }
+    )
+    res = md.convert(text)
+    return res
 
+log_message = []
+#log_message = [{"uid": 1, "message": message[{"role": "user","content": user_input},]},]
 @login_required(login_url='user:login')
 def gpt(request):
     user = User.objects.get(username=request.user.username)
@@ -24,16 +42,35 @@ def gpt(request):
         json_data = json.loads(request.body.decode("utf-8"))
         user_input = json_data.get("user_input")
         user_message_dict = {"role": "user","content": user_input}
-        messages.append(user_message_dict)
-        response=openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
+        have_user_log_message = 0
+        response = []
+        for i in log_message:
+            if i["uid"] == user.id:
+                have_user_log_message = 1
+                i["message"].append(user_message_dict)
+                response = openai.ChatCompletion.create(model="gpt-4",messages=i["message"])
+                break
+        if have_user_log_message == 0:
+            message = []
+            message.append(user_message_dict)
+            log_message.append({"uid": user.id, "message": message})
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=message)
+
         reply = response["choices"][0]["message"]["content"]
         ai_message_dict = {"role": "assistant", "content": reply}
-        messages.append(ai_message_dict)
-        if len(messages) > 8:
-            del(messages[0])
-            del(messages[0])
+
+        for i in log_message:
+            if i["uid"] == user.id:
+                i["message"].append(ai_message_dict)
+                break
+
+        for i in log_message:
+            if len(i["message"]) > 4:
+                del(i["message"][0])
+                del(i["message"][0])
+
         AGroup_Chat = {
             "user_input": user_input,
-            "ai_reply": reply,
+            "ai_reply": mdconv(reply),
         }
         return JsonResponse(AGroup_Chat)
