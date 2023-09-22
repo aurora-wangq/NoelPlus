974d38cedb80ae7cf900191b6e42fbf0f6247a82
aenstarax <12995192+aenstarax@user.noreply.gitee.com>
2023-09-22 13:55:26 +0800

    UploadChatGpt

--- gpt/views.py
+++ gpt/views.py
@@ -42,7 +42,7 @@ def gpt(request: HttpRequest):
 
         input = request.POST
         if input['model'] == 'gpt-4' and 'gpt4_permitted' not in [ x.name for x in user.groups.all() ]:
-            return HttpResponseForbidden('access denied,please hit AenStarAX or MoveToEx')
+            return HttpResponseForbidden('提问被驳回,原因是没有权限')
         
         msg = {
             "role": "user",
